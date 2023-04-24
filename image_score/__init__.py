9a5732fe27b9561b636fe6b3939ec8b09e224f50
Crystal Wang <98052760+niuniujiaojiao@users.noreply.github.com>
2023-04-24 17:34:54 -0400

    added calculate_distortion direct function

--- image_score/__init__.py
+++ image_score/__init__.py
@@ -6,5 +6,5 @@ from .api import load_image, make_grayscale, crop, display_image, display_faces,
     calculate_blur, blur_score, \
     find_faces, find_closest_face, face_score, \
     get_face_groups, composition_score, \
-    distortion_score, \
+    distortion_score, calculate_distortion \
     score
