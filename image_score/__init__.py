9bdb80525d143f7671fd9ff692c7f7d9b9a323f6
Crystal Wang <crystalwanghy@gmail.com>
2023-04-30 05:04:12 -0400

    added batch processing capabilities to image_score

--- image_score/__init__.py
+++ image_score/__init__.py
@@ -5,6 +5,6 @@ __version__ = '0.1'
 from .api import load_image, make_grayscale, crop, display_image, display_faces, display_section, \
     calculate_blur, blur_score, \
     find_faces, find_closest_face, face_score, \
-    get_face_groups, composition_score, \
-    distortion_score, calculate_distortion, \
-    score
+    get_face_groups, calculate_composition, composition_score, \
+    calculate_distortion, distortion_score, \
+    score_individual, score
