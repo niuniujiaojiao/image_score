1fafd6c0fa2674a32b553b4ccb305ae6e6180df2
Crystal Wang <98052760+niuniujiaojiao@users.noreply.github.com>
2023-05-10 01:00:41 -0400

    corrected url in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='image_score',
       version='0.1',
       description='Compare images containing people with generated images meant to resemble the original',
-      url='https://github.com/niuniujiaojiao/stable-diffusion-490/tree/main/code/final/image_score',
+      url='https://github.com/niuniujiaojiao/image_score',
       author='niuniujiaojiao',
       author_email='crystal.wang@yale.edu',
       license='',
